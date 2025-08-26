034a8099aac4d1e140e26cc430b627a2fdc44d3c
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-26 15:45:44 +0530

    hel

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:perplexity_clone/pages/home_page.dart';
 import 'package:perplexity_clone/theme/colors.dart';
-sda
 
-s
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Xeno',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Appcolors.background,
+        colorScheme: ColorScheme.formSeed(seedColor: AppColors.submitButton),
+        textTheme:  GoogleFonts.interTextTheme(
+          ThemeData.dark().textTheme.copyWith(
+            bodyMedium: const TextStyle(
+              fontSize: 15,
+              color: AppColors.whiteColor,
+            ),
+          ),
+        ),
+      ),
+      home: HomePage();
+    );
+  }
+
+}
+
