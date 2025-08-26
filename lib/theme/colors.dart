1713d446b643e4d0dc7a9b8d25c8bcc0ac094098
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-27 00:53:23 +0530

    hel

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const background=colors.fromRGBO(25,26,26,1);
-  static const sideNav = color.fromRGBO(32,34,34,1);
-  static const searchBar=color.fromRGBO(32,34,34,1);
-  static const searchBarBorder = color.fromRGBO(60,63,64,1);
-  static const iconGrey =color(0xFF909090);
-  static const textGrey=color(0xFFAAAAAA);
-  static footerGrey = color.fromRGBO(47,48,47,1);
-  static proButton =color(0xFF262626);
-  static cardColor =Color.fromRGBO(27,185,206,1);
-  static const WhiteColor =Colors.white;
+  static const background = Color.fromRGBO(25, 26, 26, 1);
+  static const sideNav = Color.fromRGBO(32, 34, 34, 1);
+  static const searchBar = Color.fromRGBO(32, 34, 34, 1);
+  static const searchBarBorder = Color.fromRGBO(60, 63, 64, 1);
+  static const iconGrey = Color(0xFF909090);
+  static const textGrey = Color(0xFFAAAAAA);
+  static const footerGrey = Color(0xFF737373);
+  static const proButton = Color.fromRGBO(47, 48, 47, 1);
+  static const cardColor = Color(0xFF262626);
+  static const submitButton = Color.fromRGBO(27, 185, 206, 1);
+  static const whiteColor = Colors.white;
 }
