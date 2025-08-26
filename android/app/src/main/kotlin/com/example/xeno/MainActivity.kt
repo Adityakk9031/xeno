3a342a7a627e5b089b91c134b95b7b567d3be410
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-26 14:01:55 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/xeno/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.xeno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:perplexity_clone/pages/home_page.dart';
+import 'package:perplexity_clone/theme/colors.dart';
+
+
