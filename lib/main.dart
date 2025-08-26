27f1776f054dba20b9736ff6a57515bb8cfead91
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-26 15:49:14 +0530

    hel

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,22 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:perplexity_clone/pages/home_page.dart';
 import 'package:perplexity_clone/theme/colors.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  Widget build(BuildContext context){
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Xeno',
       theme: ThemeData(
-        scaffoldBackgroundColor: Appcolors.background,
-        colorScheme: ColorScheme.formSeed(seedColor: AppColors.submitButton),
-        textTheme:  GoogleFonts.interTextTheme(
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.submitButton),
+        textTheme: GoogleFonts.interTextTheme(
           ThemeData.dark().textTheme.copyWith(
             bodyMedium: const TextStyle(
               fontSize: 15,
@@ -25,9 +27,7 @@ class MyApp extends StatelessWidget{
           ),
         ),
       ),
-      home: HomePage();
+      home: HomePage(),
     );
   }
-
 }
-
