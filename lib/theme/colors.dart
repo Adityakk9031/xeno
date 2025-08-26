b8fa6165ecaee0a53f5070a8efe1fdb7d1d92105
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-27 00:53:12 +0530

    hel

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,4 +4,11 @@ class AppColors {
   static const background=colors.fromRGBO(25,26,26,1);
   static const sideNav = color.fromRGBO(32,34,34,1);
   static const searchBar=color.fromRGBO(32,34,34,1);
+  static const searchBarBorder = color.fromRGBO(60,63,64,1);
+  static const iconGrey =color(0xFF909090);
+  static const textGrey=color(0xFFAAAAAA);
+  static footerGrey = color.fromRGBO(47,48,47,1);
+  static proButton =color(0xFF262626);
+  static cardColor =Color.fromRGBO(27,185,206,1);
+  static const WhiteColor =Colors.white;
 }
