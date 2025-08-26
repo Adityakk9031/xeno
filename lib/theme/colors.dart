0ad23c9a3b44b825ec428ee63ad2bfd593b820fb
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-26 23:50:11 +0530

    hel

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background=colors.fromRGBO(25,26,26,1);
+  static const sideNav = color.fromRGBO(32,34,34,1);
+  static const searchBar=color.fromRGBO(32,34,34,1);
+}
