19b83b5fad510b9231f5243b8568d9f7ca6318ed
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-28 16:49:11 +0530

    hel

--- lib/widgets/side_bar.dart
+++ lib/widgets/side_bar.dart
@@ -6,82 +6,83 @@ class SideBar extends StatefulWidget {
   const SideBar({super.key});
 
   @override
-  State<SideBar> createState() => _sideBarState();
+  State<SideBar> createState() => _SideBarState();
 }
 
-class _SideBarState extends  State<SideBar> {
+class _SideBarState extends State<SideBar> {
   bool isCollapsed = true;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: const Duration(milliseconds: 100),
-      width: isCollapsed ? 64:150,
+      width: isCollapsed ? 64 : 150,
       color: AppColors.sideNav,
-      children: [
-        const SizedBox(height: 16),
-        Icon(
-          Icons.auto_awesome_mosaic,
-          color: AppColors.whiteColor,
-          size:isCollapsed ? 30:60,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: isCollapsed
-                ? CrossAxisAlignment.center
-                : CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 24),
-              sideBarButton(
-                isCollapsed: isCollapsed,
-                icon: IconS.add,
-                text: "Home",
-              ),
-              SideBarButton(
-                isCollapsed: isCollapsed,
-                icon:Icons.add,
-                text: "Home",
-              ),
-              SideBarButton(
-                isCollapsed:isCollapsed,
-                icon:Icons.language,
-                text:"Discover",
-              ),
-              SideBarButton(
-                isCollapsed: isCollapsed,
-                icon:Icons.auto_awesome,
-                text: "Discover",
-              ),
-              SideBarButton(
-                isCollapsed: isCollapsed,
-                icon:Icons.cloud_outlined,
-                text:"Libary",
-              ),
-              const Spacer(),
-            ]
-          )
-        )
-        GestureDetector(
-          onTap:(){
-            setState(() {
-              isCollapsed=!isCollapsed;
-            });
-          },
-          child: AnimatedContainer(duration: const Duration(milliseconds: 100),
-          margin: EdgeInsets.symmetric(vertical:14),
-          child: Icon(
-            isCollapsed
-                  ? Icons.keyboard_arrow_right
-                  : Icons.keyboard_arrow_left,
-            color: AppColors.iconGrey,
-            size: 22,
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          Icon(
+            Icons.auto_awesome_mosaic,
+            color: AppColors.whiteColor,
+            size: isCollapsed ? 30 : 60,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: isCollapsed
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 24),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.add,
+                  text: "Home",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.search,
+                  text: "Search",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.language,
+                  text: "Spaces",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.auto_awesome,
+                  text: "Discover",
+                ),
+                SideBarButton(
+                  isCollapsed: isCollapsed,
+                  icon: Icons.cloud_outlined,
+                  text: "Library",
+                ),
+                const Spacer(),
+              ],
             ),
-    ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isCollapsed = !isCollapsed;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 100),
+              margin: EdgeInsets.symmetric(vertical: 14),
+              child: Icon(
+                isCollapsed
+                    ? Icons.keyboard_arrow_right
+                    : Icons.keyboard_arrow_left,
+                color: AppColors.iconGrey,
+                size: 22,
+              ),
             ),
-            const SizedBox(height: 16),
-          ))
-        )
-      ]
-    )
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
   }
 }
