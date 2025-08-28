f99df21c2ecf5d8491b2c79be3bc16d153f09caf
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-28 15:09:52 +0530

    hel

--- lib/widgets/search_section.dart
+++ lib/widgets/search_section.dart
@@ -8,30 +8,31 @@ import 'package:xeno/widgets/search_bar_button.dart';
 class SearchSection extends StatefulWidget {
   const SearchSection({super.key});
 
-  @overrideState<SearchSection> createState() => _SearchSectionState();
+  @override
+  State<SearchSection> createState() => _SearchSectionState();
 }
 
-class _searchsection extends state<_searchsection> {
-  final querycontroller =TextEditingController();
+class _SearchSectionState extends State<SearchSection> {
+  final queryController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
-    querycontroller.dispose();
+    queryController.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          'where knowledge begins' ,
-          style: GoogleFonts.ibmPleMono(
-            fontSize:40,
-            fontWeight:FontWeight.w400,
-            height:1.2,
-            letterSpacing:0.5
+          'Where knowledge begins',
+          style: GoogleFonts.ibmPlexMono(
+            fontSize: 40,
+            fontWeight: FontWeight.w400,
+            height: 1.2,
+            letterSpacing: -0.5,
           ),
         ),
         const SizedBox(height: 32),
@@ -39,8 +40,9 @@ class _searchsection extends state<_searchsection> {
           width: 700,
           decoration: BoxDecoration(
             color: AppColors.searchBar,
+            borderRadius: BorderRadius.circular(8),
             border: Border.all(
-              color: Appcolor.searchBarBorder,
+              color: AppColors.searchBarBorder,
               width: 1.5,
             ),
           ),
@@ -49,41 +51,63 @@ class _searchsection extends state<_searchsection> {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: TextField(
-                  controller: querycontroller,
+                  controller: queryController,
                   decoration: InputDecoration(
                     hintText: 'Search anything...',
-                    color: AppColors.textGrey,
-                    fontSize: 16,
+                    hintStyle: TextStyle(
+                      color: AppColors.textGrey,
+                      fontSize: 16,
+                    ),
+                    border: InputBorder.none,
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
                   ),
-                  border: InputBorder.none,
-                  isDense: true,
-                  contentPadding: EdgeInsets.zero,
                 ),
               ),
-    ),
-    Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: Row(
-    children: [
-      SerachBarButton(
-    icon: Icons.auto_awesome_outlined,
-    text: 'Attach',
-    ),
-    const Spacer(),
-    SearchBarButton(icon: Icons.add_circle_outline_outlined, text: 'Attach')
-    const Spacer(),
-    GestureDetector(
-    onTap: () {
-      chatWebService().chat(querycontroller.text.trim())
-    }
-    )
-    ]
-    )
-    )
-            ]
-          )
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    SearchBarButton(
+                      icon: Icons.auto_awesome_outlined,
+                      text: 'Focus',
+                    ),
+                    const SizedBox(width: 12),
+                    SearchBarButton(
+                      icon: Icons.add_circle_outline_outlined,
+                      text: 'Attach',
+                    ),
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        ChatWebService().chat(queryController.text.trim());
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ChatPage(question: queryController.text.trim()),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(9),
+                        decoration: BoxDecoration(
+                          color: AppColors.submitButton,
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: AppColors.background,
+                          size: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         )
-      ]
-    )
+      ],
+    );
   }
 }
