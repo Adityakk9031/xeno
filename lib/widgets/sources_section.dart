b5b870cc81791de1ac630d3e8db92e0d2dbfcda8
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-28 20:53:48 +0530

    hel

--- lib/widgets/sources_section.dart
+++ lib/widgets/sources_section.dart
@@ -3,19 +3,20 @@ import 'package:xeno/services/chat_web_service.dart';
 import 'package:xeno/theme/colors.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class SourceSection extends StatefulWidget {
-  const SourceSection({super.key});
+class SourcesSection extends StatefulWidget {
+  const SourcesSection({super.key});
 
   @override
-  State<SourceSection> createState() => _SourceSectionState();
+  State<SourcesSection> createState() => _SourcesSectionState();
 }
 
-class _SourceSectionState extends State<SourceSection> {
+class _SourcesSectionState extends State<SourcesSection> {
   bool isLoading = true;
-  List searchResults= [
+  List searchResults = [
     {
-      'title': 'Ind vs Aus Score 4th Test',
-      'url': 'https://www.moneycontrol.com/sports/cricket/ind-vs-aus-live-score-4th-test-shubman-gill-dropped-australia-win-toss-opt-to-bat-liveblog-12897631.html',
+      'title': 'Ind vs Aus Live Score 4th Test',
+      'url':
+      'https://www.moneycontrol.com/sports/cricket/ind-vs-aus-live-score-4th-test-shubman-gill-dropped-australia-win-toss-opt-to-bat-liveblog-12897631.html',
     },
     {
       'title': 'Ind vs Aus Live Boxing Day Test',
@@ -26,76 +27,82 @@ class _SourceSectionState extends State<SourceSection> {
       'title': 'Ind vs Aus - 4 Australian Batters Score Half Centuries',
       'url':
       'https://economictimes.indiatimes.com/news/sports/ind-vs-aus-four-australian-batters-score-half-centuries-in-boxing-day-test-jasprit-bumrah-leads-indias-fightback/articleshow/116674365.cms',
-    }
+    },
   ];
 
   @override
   void initState() {
     super.initState();
-    chatWebService().searchResultStream.listen((data) {
+    ChatWebService().searchResultStream.listen((data) {
       setState(() {
-        searchResults=data['data'];
-        isLoading=false;
+        searchResults = data['data'];
+        isLoading = false;
       });
-    })
+    });
   }
 
   @override
-  widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Icon(
-          Icons.source_outlined,
-          color: Color.white70,
+        Row(
+          children: [
+            Icon(
+              Icons.source_outlined,
+              color: Colors.white70,
+            ),
+            SizedBox(width: 8),
+            Text(
+              "Sources",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
         ),
-        SizedBox(width: 8),
-        Text(
-          "Sources",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          )
+        const SizedBox(height: 16),
+        Skeletonizer(
+          enabled: isLoading,
+          child: Wrap(
+            spacing: 16,
+            runSpacing: 16,
+            children: searchResults.map((res) {
+              return Container(
+                width: 150,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: AppColors.cardColor,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      res['title'],
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      res['url'],
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         )
       ],
-    )
-        const SizedBox(height:16),
-    Skeletonizer(
-    enabled: isLoading,
-    child: Wrap(
-    spacing: 16,
-    runSpacing: 16,
-    children: searchResults.map((res) {
-      return Container(
-      width: 150,
-      padding:EdgeInsets.all(16),
-    decoration: BoxDecoration(
-    color:AppColors.cardcolor,
-    borderRadius: BorderRadius.circular(8),
-    ),
-    child: column(
-    childreen: [
-      Text(
-    res["title"],
-    style:TextStyle(
-    fontWeight:FontWeight.w500,
-    ),
-    const SizedBox(height: 8),
-    Text(
-    res['url'],
-    style: TextStyle(
-    color: Colors.grey,
-    fontSize: 12,
-    ),
-    maxlines:1,
-    overflow: TextOverflow.ellipsis,
-    )
-    )
-    ]
-    )
-      )
-    }),
-    )
-    )
+    );
   }
 }
