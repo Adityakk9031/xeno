eafa51c54c1035b4124f08c5dae2cc32bfad2b57
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-28 15:19:53 +0530

    hel

---
+++ lib/widgets/side_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:xeno/theme/colors.dart';
+import 'package:xeno/widgets/side_bar_button.dart';
+
+class SideBar extends StatefulWidget {
+  const SideBar({super.key});
+
+  @override
+  State<SideBar> createState() => _sideBarState();
+}
+
+class _SideBarState extends  State<SideBar> {
+  bool isCollapsed = true;
+
+  @override
+  Widget build(BuildContext context){
+    return AnimatedContainer
+  }
+}
