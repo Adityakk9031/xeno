294fd18f865bde33f6d2d924bab9c459d16eb5ab
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2025-08-28 20:51:10 +0530

    hel

--- lib/widgets/sources_section.dart
+++ lib/widgets/sources_section.dart
@@ -27,5 +27,75 @@ class _SourceSectionState extends State<SourceSection> {
       'url':
       'https://economictimes.indiatimes.com/news/sports/ind-vs-aus-four-australian-batters-score-half-centuries-in-boxing-day-test-jasprit-bumrah-leads-indias-fightback/articleshow/116674365.cms',
     }
-  ]
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    chatWebService().searchResultStream.listen((data) {
+      setState(() {
+        searchResults=data['data'];
+        isLoading=false;
+      });
+    })
+  }
+
+  @override
+  widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Icon(
+          Icons.source_outlined,
+          color: Color.white70,
+        ),
+        SizedBox(width: 8),
+        Text(
+          "Sources",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          )
+        )
+      ],
+    )
+        const SizedBox(height:16),
+    Skeletonizer(
+    enabled: isLoading,
+    child: Wrap(
+    spacing: 16,
+    runSpacing: 16,
+    children: searchResults.map((res) {
+      return Container(
+      width: 150,
+      padding:EdgeInsets.all(16),
+    decoration: BoxDecoration(
+    color:AppColors.cardcolor,
+    borderRadius: BorderRadius.circular(8),
+    ),
+    child: column(
+    childreen: [
+      Text(
+    res["title"],
+    style:TextStyle(
+    fontWeight:FontWeight.w500,
+    ),
+    const SizedBox(height: 8),
+    Text(
+    res['url'],
+    style: TextStyle(
+    color: Colors.grey,
+    fontSize: 12,
+    ),
+    maxlines:1,
+    overflow: TextOverflow.ellipsis,
+    )
+    )
+    ]
+    )
+      )
+    }),
+    )
+    )
+  }
 }
